f7c23e422b37df4459123d18a0210e971c432027
Tino Schr <tino.schreiber@t-systems.com>
2023-06-02 12:14:01 +0200

    Update tox.ini && conf.py file (#24)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: SebastianGode

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,6 +28,15 @@ project = 'Tag Management Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/tag-management-service'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/tag-management-service/api-ref/'
+otcdocs_doc_title = 'API Reference'
+otcdocs_doc_type = 'api-ref'
+otcdocs_service_category = 'md'
+otcdocs_service_title = 'Tag Management Service'
+otcdocs_service_type = 'tms'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
